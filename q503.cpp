89f30add9f21583d1b0c4ad893f6eb607dd388c2
Nikhil Behera <NikhilBeheraTech@gmail.com>
2025-05-21 10:03:30 +0530

    q503 half

---
+++ q503.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        // vector<int> res;
+        int n= nums.size();
+        int max=nums[0];
+        for(int i=0;i<n;i++){
+            if(nums[i]>max){
+                max = nums[i];
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(nums[i]<max){
+                while(nums[i]!=max){
+                    nums[i] = nums[i]+1;
+                }
+            }
+            else{
+                nums[i] =-1;
+            }
+        }
+    }
+};
+                // int k = max-nums[i];      this was another trial approach.
+                // for(int j=0;j<k;j++){
+                //     nums[i] = nums[i]+1;
+                // }
