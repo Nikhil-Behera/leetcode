e87ad7c5bdca1d6626ad92dc0ac9ea11ad4dad97
Nikhil Behera <NikhilBeheraTech@gmail.com>
2025-08-03 15:31:55 +0530

    optimized 1464

--- q1464.cpp
+++ q1464.cpp
@@ -1,21 +1,38 @@
 /*Given the array of integers nums, you will choose two different indices i and j of that array. Return the maximum value of (nums[i]-1)*(nums[j]-1).*/
 #include<iostream>
 #include<algorithm>
+#include<queue>
 #include<vector>
 using namespace std;
+// class Solution {
+// public:
+//     int maxProduct(vector<int>& nums) {
+//         sort(nums.begin(),nums.end());
+//         int n = nums.size();
+//         int max = 0;
+//         for(int i=0;i<n;i++){
+//             for(int j=i+1;j<n;j++){
+//                 if((nums[i]-1)*(nums[j]-1) > max){
+//                     max = (nums[i]-1)*(nums[j]-1);
+//                 }
+//             }
+//         }
+//         return max;
+//     }
+// };
+
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int n = nums.size();
-        int max = 0;
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if((nums[i]-1)*(nums[j]-1) > max){
-                    max = (nums[i]-1)*(nums[j]-1);
-                }
-            }
+        priority_queue<int> pq;
+        for(auto it : nums){
+            pq.push(it);
         }
-        return max;
+        int x = pq.top() - 1;
+        pq.pop();
+        int y = pq.top() - 1;
+        pq.pop();
+
+        return x*y;
     }
 };
