1cad80e5b00476db5fed9ad41e7288301f77e848
Nikhil Behera <NikhilBeheraTech@gmail.com>
2025-11-09 11:34:58 +0530

    added 2169(daily)

---
+++ q2169.cpp
@@ -0,0 +1,30 @@
+/*You are given two non-negative integers num1 and num2.
+
+In one operation, if num1 >= num2, you must subtract num2 from num1, otherwise subtract num1 from num2.
+
+For example, if num1 = 5 and num2 = 4, subtract num2 from num1, thus obtaining num1 = 1 and num2 = 4. However, if num1 = 4 and num2 = 5, after one operation, num1 = 4 and num2 = 1.
+Return the number of operations required to make either num1 = 0 or num2 = 0.*/
+
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+    int countOperations(int num1, int num2) {
+        int count=0;
+        while(num1 != 0 && num2 != 0){
+            if(num1 > num2){
+                num1 -= num2;
+                count++;
+            }
+            else if(num1 < num2){
+                num2 -= num1;
+                count++;
+            }
+            else if(num1 == num2){
+                num1 -= num2;
+                count++;
+            }
+        }
+        return count;
+    }
+};
