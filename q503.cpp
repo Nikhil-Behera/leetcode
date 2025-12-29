0499384afccb6938bea9d5fdcfaed50d64f6407e
Nikhil Behera <NikhilBeheraTech@gmail.com>
2025-12-29 13:39:47 +0530

    done q 503 with correct approach ; done 1351

--- q503.cpp
+++ q503.cpp
@@ -1,7 +1,8 @@
 #include<iostream>
+#include<stack>
 #include<vector>
 using namespace std;
-class Solution {
+/*class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
         // vector<int> res;
@@ -27,4 +28,26 @@ public:
                 // int k = max-nums[i];      this was another trial approach.
                 // for(int j=0;j<k;j++){
                 //     nums[i] = nums[i]+1;
-                // }
+                // }*/
+
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n = nums.size();
+        stack<int> st;
+        vector<int> res(n,-1);
+
+        for(int i=2*n-1;i>=0;i--){
+            int index = i%n;
+            while(!st.empty() && nums[index] >= st.top()){
+                st.pop();
+            }
+            if(!st.empty()){
+                res[index] = st.top();
+            }
+            st.push(nums[index]);
+        }
+        
+        return res;
+    }
+};
