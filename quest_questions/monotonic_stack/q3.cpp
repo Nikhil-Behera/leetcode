09e78408f33611dd39118c621a7b7765e17f6755
Nikhil Behera <NikhilBeheraTech@gmail.com>
2026-03-18 19:54:48 +0530

    added lc after a long time

--- quest_questions/monotonic_stack/q3.cpp
+++ quest_questions/monotonic_stack/q3.cpp
@@ -3,3 +3,33 @@
 #include<vector>
 #include<stack>
 using namespace std;
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+        stack<int> st;
+        int maxar = 0;
+        int area=0;
+        // int smol=0;
+
+        for(int i=0;i<=n;i++){
+            int current_height = (i==n) ? 0 : heights[i]; 
+            while(!st.empty() && heights[st.top()] > current_height){
+                // smol = st.top();
+                int height = heights[st.top()];
+                st.pop();
+                
+                int width;
+                if(st.empty()){
+                    width = i;
+                }
+                else{
+                    width = i-st.top()-1;
+                }
+                maxar = max(maxar,height*width);
+            }
+            st.push(i);
+        }
+        return maxar;
+    }
+};
