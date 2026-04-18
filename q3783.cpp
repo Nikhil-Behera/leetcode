eef2a52d340a0755caeb6415aff5671b9536e7b1
Nikhil Behera <NikhilBeheraTech@gmail.com>
2026-04-18 19:21:00 +0530

    feat: add solution for mirror distance calculation in q3783.cpp

---
+++ q3783.cpp
@@ -0,0 +1,30 @@
+/*You are given an integer n.
+
+Define its mirror distance as: abs(n - reverse(n))​​​​​​​ where reverse(n) is the integer formed by reversing the digits of n.
+
+Return an integer denoting the mirror distance of n​​​​​​​.
+
+abs(x) denotes the absolute value of x.*/
+#include<bits/stdc++>
+using namespace std;
+class Solution {
+public:
+    int mirrorDistance(int n) {
+        int revn=0;
+        int oldn = n;
+        while(n > 0){
+            int extracted = n%10;
+            if(revn == 0){
+                revn = extracted;
+            }
+            else{
+                revn *= 10;
+                revn += extracted;
+            }
+            n /= 10;
+        }
+        int ans = 0;
+        ans = abs(oldn - revn);
+        return ans;
+    }
+};
