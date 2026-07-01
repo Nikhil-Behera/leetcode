b017a74bba1682a6b1855c98afe8561707310cb7
Nikhil Behera <NikhilBeheraTech@gmail.com>
2026-07-01 14:50:27 +0530

    added the questions done in bootcamp

--- q75.cpp
+++ q75.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<map>
 using namespace std;
 class Solution {
 public:
@@ -8,15 +9,37 @@ public:
         // for(int i=0;i<nums.size();i++){
         //     cout<<nums[i];
         // }
-        int n=nums.size();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n-i-1;j++){
-                if(nums[j]>nums[j+1]){
-                    swap(nums[j],nums[j+1]);
-                }
+
+
+        // int n=nums.size();
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<n-i-1;j++){
+        //         if(nums[j]>nums[j+1]){
+        //             swap(nums[j],nums[j+1]);
+        //         }
+        //     }
+        // }
+        // for(int i=0;i<nums.size();i++){
+        //     cout<<nums[i];}
+
+        
+
+            int size = nums.size();
+
+            map<int,int> mp;
+
+            for(auto it : nums){
+                mp[it]++;
             }
-        }
-        for(int i=0;i<nums.size();i++){
-            cout<<nums[i];}
+
+            int i=0;
+            for(auto it : mp){
+                for(int j=0;j<it.second;j++){
+                    nums[i] = it.first;
+                    i++;
+                }  
+            }
+
+        
     }
 };
