e18568cce092494812e30280f921ae9337743341
Nikhil Behera <NikhilBeheraTech@gmail.com>
2026-07-21 20:46:45 +0530

    q 3658 done

---
+++ q3658.cpp
@@ -0,0 +1,15 @@
+/*You are given an integer n. Your task is to compute the GCD (greatest common divisor) of two values:
+
+sumOdd: the sum of the smallest n positive odd numbers.
+
+sumEven: the sum of the smallest n positive even numbers.
+
+Return the GCD of sumOdd and sumEven.*/
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+};
